d680f16483c9c4b3f4d2fa0811abe34a733e5999
sergei_kolbasin <georgio1997@mail.ru>
2023-11-27 18:55:16 +0300

    first commit

--- mysite/main_list/urls.py
+++ mysite/main_list/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from main_list.views import main_list_view
+from main_list.views import main_list_view, hi_view
 
 app_name = "main_list"
 
 urlpatterns = [
     path("", main_list_view, name="main"),
+    path("/hi/", hi_view, name='hi'),
 ]
